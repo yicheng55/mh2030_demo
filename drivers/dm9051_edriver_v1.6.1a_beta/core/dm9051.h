e73117b1e3d54c6fc43e8cf9f644125eb47b0aba
yich_liao <yich_liao@davicom.com.tw>
2026-06-04 16:18:33 +0800

    Update .gitignore and project files for improved organization and path corrections

--- drivers/dm9051_edriver_v1.6.1a_beta/core/dm9051.h
+++ drivers/dm9051_edriver_v1.6.1a_beta/core/dm9051.h
@@ -9,6 +9,11 @@ extern "C" {
 #endif
 
 #define PHY_STATUS_REG            (0x01)           /*!< basic mode status register */
+#define DM9051_VIDL               (0x28)
+#define DM9051_VIDH               (0x29)
+#define DM9051_PIDL               (0x2A)
+#define DM9051_PIDH               (0x2B)
+#define DM9051_CHIPR              (0x2C)
 #define DM9051_MRCMD              (0x72)
 #define DM9051_MWCMD              (0x78)
 #define OPC_REG_W                 0x80             // Register Write
