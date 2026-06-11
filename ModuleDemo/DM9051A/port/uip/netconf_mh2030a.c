d9657b7cabe4b021be9d85c468ed0af4e5c6ffb6
yich_liao <yich_liao@davicom.com.tw>
2026-06-11 16:51:45 +0800

    Refactor DM9051 uIP stack: update project configuration to remove unused periodic definition and restore time update function

--- ModuleDemo/DM9051A/port/uip/netconf_mh2030a.c
+++ ModuleDemo/DM9051A/port/uip/netconf_mh2030a.c
@@ -33,12 +33,12 @@ void resolv_found(char *name, u16_t *ipaddr)
 }
 
 // Moved to mh2030a_uip_clock.c to avoid duplicate definition
-// void mh2030a_uip_update_time(void)
-// {
-//     uip_elapsed_ms += MH2030A_UIP_TICK_MS;
-// //    g_RunTime += MH2030A_UIP_TICK_MS;
-// //    all_local_time += MH2030A_UIP_TICK_MS;
-// }
+ void mh2030a_uip_update_time(void)
+ {
+     uip_elapsed_ms += MH2030A_UIP_TICK_MS;
+ //    g_RunTime += MH2030A_UIP_TICK_MS;
+ //    all_local_time += MH2030A_UIP_TICK_MS;
+ }
 
 void mh2030a_uip_net_init(void)
 {
